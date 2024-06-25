a61aee3851a3b5dd538901a4c2de6d41e80c4978
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-06-25 20:03:36 +0700

    perbaiki cors

--- config/cors.go
+++ config/cors.go
@@ -30,19 +30,38 @@ var Headers = []string{
 	"X-Requested-With",
 }
 
-func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
-	// Set CORS headers for the preflight request
-	if r.Method == http.MethodOptions {
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
-		w.Header().Set("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE")
-		w.Header().Set("Access-Control-Allow-Origin", "https://parkirgratis.github.io")
-		w.Header().Set("Access-Control-Max-Age", "3600")
-		w.WriteHeader(http.StatusNoContent)
-		return true
+// Fungsi untuk memeriksa apakah origin diizinkan
+func isAllowedOrigin(origin string) bool {
+	for _, o := range Origins {
+		if o == origin {
+			return true
+		}
 	}
-	// Set CORS headers for the main request.
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.Header().Set("Access-Control-Allow-Origin", "https://parkirgratis.github.io")
+	return false
+}
+
+// Fungsi untuk mengatur header CORS
+func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
+	origin := r.Header.Get("Origin")
+
+	if isAllowedOrigin(origin) {
+		// Set CORS headers for the preflight request
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
+			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE")
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Origin", "https://parkirgratis.github.io")
+			w.Header().Set("Access-Control-Max-Age", "3600")
+			w.WriteHeader(http.StatusNoContent)
+			return true
+		}
+		// Set CORS headers for the main request.
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Origin", "https://parkirgratis.github.io")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		return false
+	}
+
 	return false
 }
